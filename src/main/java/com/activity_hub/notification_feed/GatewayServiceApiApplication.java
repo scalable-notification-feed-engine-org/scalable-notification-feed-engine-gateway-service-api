92f8239041c39ca2fdd3c61d4212c37e7654d9dd
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-17 10:13:16 +0530

    Init commit👍...

---
+++ src/main/java/com/activity_hub/notification_feed/GatewayServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.activity_hub.notification_feed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/activity_hub/notification_feed/GatewayServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.activity_hub.notification_feed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
